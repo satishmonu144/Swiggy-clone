0dddea7fd3d0d3682615032cf89d05d838ec419e
satish kumar <satishmonu144@gmail.com>
2023-08-08 00:33:41 +0530

    model and router commit

--- backend2/db.js
+++ backend2/db.js
@@ -1,22 +1,23 @@
 const mongoose = require("mongoose");
+const mongoUrl = 'mongodb://127.0.0.1:27017/swiggy'; // Corrected the URL format
 
-const mongodb = async() => {
- await mongoose.connect('mongodb://127.0.0.1:27017/swiggy', {
-    useNewUrlParser: true,
-   useUnifiedTopology: true,
-  })
-  .then(async() => {
+const mongodb = async () => {
+  try {
+    await mongoose.connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log("Connected to MongoDB");
+
     const fetched_data = await mongoose.connection.db.collection("restaurant");
-    fetched_data.find({}).toArray(function(err,data)
-    {
-        if(err) console.log(err);
-       else console.log(err);
-    })
-  })
-  .catch((error) => {
+    fetched_data.find({}).toArray(function (err, data) {
+      if (err) console.log(err);
+      else console.log(data);
+    });
+  } catch (error) {
     console.error("Error connecting to MongoDB:", error);
-  });
-}
+  }
+};
 
 module.exports = mongodb;
